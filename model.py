3a800362eefddf91d0010c1ec0fad42770bc1e4b
NUTSHELL <1220696790@qq.com>
2022-12-09 19:59:04 +0800

    221209

--- model.py
+++ model.py
@@ -129,6 +129,7 @@ class Generator(nn.Module):
         self.w_c=w_c
         self.steps=steps
         self.map_net=Mapping(z_dim=w_c)
+        self.sigmoid=nn.Sigmoid()
         for i in range(steps+1):
             c=self.n_channels[i]
             to_rgb=EqualizedConv(c,3,kernel_size=1,padding=0)
@@ -138,7 +139,7 @@ class Generator(nn.Module):
             self.g_blocks.append(G_block(c,c,w_c,need_upsample=False))
         for i in range(steps):
             self.up_samples.append(nn.Upsample(scale_factor=2))
-
+       
     def forward(self,inputs):
         x,w=inputs
         w=self.map_net(w)
@@ -158,7 +159,7 @@ class Generator(nn.Module):
                 rgb_out=self.up_samples[i-1](rgb_out)
                 rgb_out+=self.to_rgbs[i](x)
 
-        return rgb_out
+        return self.sigmoid(rgb_out)
 
 class D_block(nn.Module):
     def __init__(self,in_c,out_c):
@@ -212,7 +213,7 @@ class Discriminator(nn.Module):
         x=self.dense0(x)
         x=self.leaky(x)
         x=self.dense1(x)
-        x=self.sigmoid(x)
+        # x=self.sigmoid(x)
         return x
 
 if __name__=='__main__':
--- train.py
+++ train.py
@@ -15,6 +15,8 @@ batch_size=4
 start_res=(5,8)
 start_c=256
 w_c=512
+save_images=True
+auto_scale=True
 save_path='MC_saved.pt'
 device='cuda' if torch.cuda.is_available() else "cpu"
 gen=Generator(start_res=start_res,w_c=w_c,start_c=start_c).to(device)
@@ -34,15 +36,17 @@ if os.path.exists(save_path):
 else:
     print('initializing from scratch')
 
-loss_fn=nn.BCELoss()
+loss_fn=nn.BCEWithLogitsLoss()
 base_tensor=torch.ones((batch_size,start_c,start_res[0],start_res[1])).to(device)
+if auto_scale:
+    scaler_disc = torch.cuda.amp.GradScaler()
+    scaler_gen = torch.cuda.amp.GradScaler()
 def train_fn(epochs):
     for i in range(epochs):
         dataloader=create_dataloader((320,512),batch_size)
         for idx,(real,labels) in tqdm(enumerate(dataloader)):
             #train disc
             real=real.to(device)
-            # print('real shape:',real.shape)
             latent=torch.randn(batch_size,w_c).to(device)
             fake=gen([base_tensor,latent])
             opt_disc.zero_grad()
@@ -53,6 +57,7 @@ def train_fn(epochs):
             print('d_loss:',d_loss.item())
             d_loss.backward()
             opt_disc.step()
+            # print("1:",next(iter(disc.parameters())).data)
             #train gen
             opt_gen.zero_grad()
             # g_loss=-torch.mean(disc(fake))
@@ -60,15 +65,44 @@ def train_fn(epochs):
             print('g_loss',g_loss.item())
             g_loss.backward()
             opt_gen.step()
-            # latent=torch.randn(batch_size,w_c).to(device)
-            # fake=gen([base_tensor,latent],5)
+        
+        if save_images:
+            save_image(fake.cpu().detach()[0],f'generated_MC_landscapes/generated_img_{total_epochs+i}.jpg')
 
-        save_image(fake.cpu().detach()[0],f'generated_MC_landscapes/generated_img_{total_epochs+i}.jpg')
-
-
+def train_fn_auto_scaler(epochs):
+    for i in range(epochs):
+        dataloader=create_dataloader((320,512),batch_size)
+        for idx,(real,labels) in tqdm(enumerate(dataloader)):
+            #train disc
+            real=real.to(device)
+            latent=torch.randn(batch_size,w_c).to(device)
+            opt_disc.zero_grad()
+            with torch.cuda.amp.autocast():
+                fake=gen([base_tensor,latent])
+                disc_real=disc(real)
+                disc_fake=disc(fake.detach())
+                # d_loss=-torch.mean(disc(real))+torch.mean(disc(fake.detach()))
+                d_loss=loss_fn(disc_real.reshape(batch_size),torch.ones(batch_size).to(device))+loss_fn(disc_fake.reshape(batch_size),torch.zeros(batch_size).to(device))
+            print('d_loss:',d_loss.item())
+            scaler_disc.scale(d_loss).backward()
+            scaler_disc.step(opt_disc)
+            scaler_disc.update()
+            # print("1:",next(iter(disc.parameters())).data)
+            #train gen
+            opt_gen.zero_grad()
+            with torch.cuda.amp.autocast():
+                # g_loss=-torch.mean(disc(fake))
+                g_loss=loss_fn(disc(fake).reshape(batch_size),torch.ones(batch_size).to(device))
+            print('g_loss',g_loss.item())
+            scaler_gen.scale(g_loss).backward()
+            scaler_gen.step(opt_gen)
+            scaler_gen.update()
+        
+        if save_images:
+            save_image(fake.cpu().detach()[0],f'generated_MC_landscapes/generated_img_{total_epochs+i}.jpg')
 
 if __name__=='__main__':
-    epochs=30
+    epochs=5
     train_fn(epochs)
     total_epochs+=epochs
     # n_examples=4
